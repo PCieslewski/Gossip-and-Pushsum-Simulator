3cccb1e9c6e090903598dd0a79739520bd0d498b
PCieslewski <Pawel.vip@gmail.com>
2015-09-29 20:31:59 -0400

    Commit 5 - Refactor

--- src/main/scala-2.11/Main.scala
+++ src/main/scala-2.11/Main.scala
@@ -4,13 +4,13 @@ object Main{
 
   def main(args: Array[String]){
 
-    //val numNodes  = args(0).toInt
-    //val topology  = args(1)
-    //val algorithm = args(2)
+    val numNodes  = args(0).toInt
+    val topology  = args(1)
+    val algorithm = args(2)
 
-    val numNodes = 3000
-    val topology = "imp3D"
-    val algorithm = "push-sum"
+    //val numNodes = 3000
+    //val topology = "imp3D"
+    //val algorithm = "push-sum"
 
     val manager = MySystem.system.actorOf(Props(new Manager(numNodes, topology, algorithm)), name = "Manager")
     manager ! new Start()
